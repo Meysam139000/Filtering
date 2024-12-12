0cea6885afab4e5850efa3f7c3a34ab5c4df6b32
Meysam139000 <meysam.mousavi029@gmail.com>
2024-12-12 15:22:58 +0330

    Update Filtering.py

--- Filtering.py
+++ Filtering.py
@@ -1,25 +1,4 @@
-#python 2.7.15
-import os
-os.system("pip install colored")
-os.system("pip install colorama")
-os.system("pip install datetime")
-#importing
-import time                                                     
-import sys                                                      
-import colored                                                  
-import colorama                                                 
-import datetime
-from colored import fg, bg, attr
-from colorama import Fore, Back, Style                          
-# rang
-red='\033[31m'
-green='\033[32m'
-blue='\033[36m'
-pink='\033[35m'
-rang='\033[34m'
 
-
-os.system("clear")
 print("I'M ' Meysam ' Tikafil")
 print("Code Filteri Rubika")
 print("Code : 0.0.0.0.0.0.0.0.0.0 ")
